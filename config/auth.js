f2b781a6189552f8c3887798923f25d74eaaa3d5
mklleung <mkleung@brandeis.edu>
2018-08-01 22:11:35 -0400

    added back auth.js

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+
+    'googleAuth' : {
+        'clientID': '978344758157-vqi641a2cihiesm05iifcvu1on9722ft.apps.googleusercontent.com',
+        'clientSecret'  : 'iHzW6Xe2Sk1hTrMoyCw8Pvmh',
+        //'callbackURL'   : 'http://127.0.0.1:3000/login/authorized'
+        'callbackURL'   : 'https://order-platform.herokuapp.com/login/authorized'
+        //'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6300/login/authorized'
+    }
+};
